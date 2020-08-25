18fa7d9f726af614f71c98ea6ca235386fec487e
eyyupsert <49327647+eyyupsert@users.noreply.github.com>
2020-08-25 15:08:18 +0300

    Personel İşlemleri
    Personel listeleme sayfası  içerisinde personel güncelle ve sil butonlarına action atandı. Güncelle buton ile birlikte personelin bilgileri bilgilerin güncelleneceği sayfaya taşınıyor. Güncelleme istenilen bilgi değiştirilebiliyor. Sil butonu ile personel veritabanından siliniyor.

--- TaskManager/Controllers/PersonelController.cs
+++ TaskManager/Controllers/PersonelController.cs
@@ -27,11 +27,43 @@ namespace TaskManager.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult YeniPersonelEkle(Personel person)
+        public IActionResult YeniPersonel(Personel person)
         {
             var per = c.departmanlars.Where(x => x.Id == person.depart.Id).FirstOrDefault();
             person.depart = per;
             c.personellers.Add(person);
+            c.SaveChanges();
+            return RedirectToAction("PersonelListele");
+        }
+        public IActionResult PersonelSil(int Id)
+        {
+            var silinecekPersonel = c.personellers.Find(Id);
+            c.personellers.Remove(silinecekPersonel);
+            c.SaveChanges();
+            return RedirectToAction("PersonelListele");
+        }
+        [HttpGet]
+        public IActionResult PersonelGuncelle(int id)
+        {
+            List<Departman> departmanList = new List<Departman>();
+            departmanList = (from x in c.departmanlars select x).ToList();
+            departmanList.Insert(0, new Departman { Id = 0, departmanAd = "-- Departman Seç --" });
+            ViewBag.departmanDeger = departmanList;
+            var GPersonel = c.personellers.Find(id );
+            return View("PersonelGuncelle", GPersonel);
+        }
+        [HttpPost]
+        public IActionResult PersonelGuncelle(Personel Vperson)
+        {
+            var Gpersonel = c.personellers.Find(Vperson.personelId);
+            var per = c.departmanlars.Where(x => x.Id == Vperson.depart.Id).FirstOrDefault();
+            Gpersonel.depart = per;
+            Gpersonel.personelAd = Vperson.personelAd;
+            Gpersonel.personelSoyad = Vperson.personelSoyad;
+            Gpersonel.iseBaslamaTarihi = Vperson.iseBaslamaTarihi;
+            Gpersonel.telNo = Vperson.telNo;
+            Gpersonel.mailAdres = Vperson.mailAdres;
+           
             c.SaveChanges();
             return RedirectToAction("PersonelListele");
         }
